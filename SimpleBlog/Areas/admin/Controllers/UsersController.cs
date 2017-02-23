c1987da458a7bc49a3408fbf6c7a5f1a40d19a80
Eray <erydmr@gmail.com>
2017-02-23 17:54:02 +0200

    23.02.2017

---
+++ SimpleBlog/Areas/admin/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: admin/Users
+        public ActionResult Index()
+        {
+            return Content("Users Index");
+        }
+
+        public ActionResult List()
+        {
+            return Content("Users List");
+        }
+
+
+        public ActionResult Edit(int id=0)
+        {
+            return Content("Users Edit :"+ id.ToString() );
+        }
+    }
+}
