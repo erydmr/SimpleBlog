2d9cffc627da44a9cfee52fd433b57c48617cbd0
Eray Demir <erydmr@gmail.com>
2017-03-06 18:27:35 +0200

    06.03.2017

---
+++ SimpleBlog/Areas/ViewModels/ViewModelsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.ViewModels
+{
+    public class ViewModelsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ViewModels";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ViewModels_default",
+                "ViewModels/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
