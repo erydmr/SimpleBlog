f51d669e95d4fb229088370382c197b7fa8887a5
Eray Demir <erydmr@gmail.com>
2017-03-09 18:12:47 +0200

    09.03.2017

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,12 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            return Content("Hi " + form.UserName + " <br>Password: " + form.Password);
         }
     }
 }
